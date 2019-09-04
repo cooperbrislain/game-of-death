a04920ded5cd1eaf053371c420ce05ca1be519b9
Cooper Brislain <cbrislain@gmail.com>
2019-09-03 18:02:24 -0700

    added fighter select and portraits

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ const Game = {
         drawSplashScreen();
     },
     playerSelect: function(which) {
-
     },
     opponentSelect: function(which) {
 
@@ -38,31 +37,51 @@ const Fighter = {
 };
 
 var Fighters = [];
+var Player;
+var Opponent;
 
 function drawSplashScreen() {
-    var $splash = $('<div>').addClass('jumbotron');
-    $('<h1>').text('WOOK FIGHT').appendTo($splash);
+    var $splash = $('<div>').addClass('jumbotron splash');
+    $('<h1>').text('GAME OF DEATH').appendTo($splash);
+    $('<img>').attr('src',`assets/img/game-of-death.png`).appendTo($splash);
     $('body').append($splash);
 }
 
 function drawFighterSelect() {
-    var $popup = $('<div>');
+    var $screen = $('<div>').addClass('jumbotron fighter-select')
+    var $deck = $('<div>').addClass('card-deck').appendTo($screen);
     for (var i=0; i<Fighters.length;i++) {
-        var $fightercard = $('<div class="card fighter-card>');
-        $('<img>').attr('src',`assets/img/${Fighters[i].portrait}`).appendTo($fighterCard);
-        $('<h3>').text(Fighters[i].name).appendTo($fighterCard);
+        var $fighterCard = $('<div>').addClass('card fighter-card');
+        $('<img>')
+            .addClass('card-img')
+            .attr('src',`assets/img/${(Fighters[i].portrait? Fighters[i].portrait : 'blank.jpg' )}`).appendTo($fighterCard);
+        $('<h3>')
+            .addClass('text-center')
+            .text(Fighters[i].name).appendTo($fighterCard);
+        $fighterCard.appendTo($deck);
     }
+    $screen.appendTo($('body'));
 }
 
 $(document).ready(function() {
     console.log('Loading Fighter Data');
     $.ajax('assets/data/fighters.json')
-        .done(function(data) {
-            console.log('Fighter Data Loaded');
-            Fighters = data;
-            Game.start();
-        })
-        .fail(function(jqXHR, textStatus) {
-            console.log(`Failure: ${textStatus}`);
-        });
+    .done(function(data) {
+        console.log('Fighter Data Loaded');
+        Fighters = data;
+        Game.start();
+    })
+    .fail(function(jqXHR, textStatus) {
+        console.log(`Failure: ${textStatus}`);
+    });
+    
+    $(document).on('click', '.splash', function() {
+        $(this).remove();
+        drawFighterSelect();
+    });
+
+    $(document).on('click', '.fighter-select .fighter-card', function() {
+        theIndex = $('.fighter-card').index($(this));
+        console.log(`Fighter selected: ${theIndex}`);
+    });
 });
